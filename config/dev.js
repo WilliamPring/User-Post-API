375e43c13f38a645f618f6e8fdfa4f03ce521b3c
WilliamPring <mrwilliampring@gmail.com>
2020-04-30 21:56:45 -0400

    Code clean up moved the comsumer to a different repo

--- config/dev.js
+++ config/dev.js
@@ -1,11 +1,6 @@
 module.exports = ({
     baseUrl: 'www.google.com',
     graphqlUrl: 'http://localhost:3004/',
-    S3Bucket: {
-        bucketName: 'foodie-s3',
-        id: '',
-        secret: ''
-    },
     //amqp://admin:pass@localhost
     Queue: {
         url: 'localhost',
